4f771da441204ecb8be3762fd88be0d5f3f2a421
stanislau.dubrouski <jeylkey@gmail.com>
2015-03-22 23:40:27 +0100

    persons listing licalized. manual locale setting added.

---
+++ src/main/java/net/dubrouski/fams/controller/LocaleManager.java
@@ -0,0 +1,32 @@
+package net.dubrouski.fams.controller;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+@ManagedBean
+@SessionScoped
+public class LocaleManager implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Locale locale = FacesContext.getCurrentInstance().getViewRoot()
+			.getLocale();
+
+	public Locale getLocale() {
+		return locale;
+	}
+
+	public String getLanguage() {
+		return locale.toString();
+	}
+
+	public void setLanguage(String language) {
+		locale = new Locale(language);
+		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
+	}
+
+}
