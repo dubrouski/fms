3eccb07d99a419e3fb93f7434c07663a6339b591
xprazak2 <oprazak@redhat.com>
2015-04-05 19:33:02 +0200

    initial contract + rest

---
+++ src/main/java/net/dubrouski/fams/model/Contract.java
@@ -0,0 +1,136 @@
+package net.dubrouski.fams.model;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Convert;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import net.dubrouski.fams.converter.LocalDatePersistenceConverter;
+import net.dubrouski.fams.model.enums.ContractState;
+
+@Entity
+public class Contract implements BaseEntity, Serializable{
+
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "ID")
+	private Long Id;
+	
+	@Column(name = "START_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate startDate;
+	
+	@Column(name = "END_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate endDate;
+	
+	@Column(name ="STATE")
+	private ContractState state;
+	
+	
+	@Column(name = "SIGN_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate signDate;
+	
+	@Column(name = "TERMINATION_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate terminationDate;
+	
+	@Column(name = "KEYS_HANDED_OVER")
+	private boolean keysHandedOver;
+	
+	@Column(name = "KEYS_HANDOVER_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate keysHandoverDate;
+	
+	@Column(name = "TERMINATION_REQUEST_DATE")
+	@Convert(converter = LocalDatePersistenceConverter.class)
+	private LocalDate terminationRequestDate;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public ContractState getState() {
+		return state;
+	}
+
+	public void setState(ContractState state) {
+		this.state = state;
+	}
+
+	public LocalDate getSignDate() {
+		return signDate;
+	}
+
+	public void setSignDate(LocalDate signDate) {
+		this.signDate = signDate;
+	}
+
+	public LocalDate getTerminationDate() {
+		return terminationDate;
+	}
+
+	public void setTerminationDate(LocalDate terminationDate) {
+		this.terminationDate = terminationDate;
+	}
+
+	public boolean isKeysHandedOver() {
+		return keysHandedOver;
+	}
+
+	public void setKeysHandedOver(boolean keysHandedOver) {
+		this.keysHandedOver = keysHandedOver;
+	}
+
+	public LocalDate getKeysHandoverDate() {
+		return keysHandoverDate;
+	}
+
+	public void setKeysHandoverDate(LocalDate keysHandoverDate) {
+		this.keysHandoverDate = keysHandoverDate;
+	}
+
+	public LocalDate getTerminationRequestDate() {
+		return terminationRequestDate;
+	}
+
+	public void setTerminationRequestDate(LocalDate terminationRequestDate) {
+		this.terminationRequestDate = terminationRequestDate;
+	}
+	
+	
+	
+	
+	
+	
+
+}
---
+++ src/main/java/net/dubrouski/fams/rest/Rest.java
@@ -0,0 +1,12 @@
+package net.dubrouski.fams.rest;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Produces("application/json")
+@Consumes("application/json")
+@Path("/v1")
+public class Rest {
+
+}
