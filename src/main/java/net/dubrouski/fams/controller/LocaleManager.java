d9958d88d05493c3977562a74034266b73840ee1
stanislau.dubrouski <jeylkey@gmail.com>
2015-04-13 23:09:26 +0200

    locale setting fixed

--- src/main/java/net/dubrouski/fams/controller/LocaleManager.java
+++ src/main/java/net/dubrouski/fams/controller/LocaleManager.java
@@ -3,6 +3,7 @@ package net.dubrouski.fams.controller;
 import java.io.Serializable;
 import java.util.Locale;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
@@ -17,8 +18,7 @@ public class LocaleManager implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	private Locale locale = FacesContext.getCurrentInstance().getViewRoot()
-			.getLocale();
+	private Locale locale;
 
 	public Locale getLocale() {
 		return locale;
@@ -33,4 +33,11 @@ public class LocaleManager implements Serializable {
 		FacesContext.getCurrentInstance().getViewRoot().setLocale(locale);
 	}
 
+	@PostConstruct
+	public void setDefaultLocale() {
+		this.locale = new Locale("en_us");
+		
+//		this.locale = FacesContext.getCurrentInstance().getViewRoot()
+//				.getLocale();
+	}
 }
