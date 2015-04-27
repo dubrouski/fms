3aa9e37b2833d1bbc124cdb955695ca50e224b85
xprazak2 <oprazak@redhat.com>
2015-04-27 16:29:29 +0200

    liating all accommodations properly

--- src/main/java/net/dubrouski/fams/dao/impl/PersonAddressDaoImpl.java
+++ src/main/java/net/dubrouski/fams/dao/impl/PersonAddressDaoImpl.java
@@ -1,12 +1,15 @@
 package net.dubrouski.fams.dao.impl;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 import javax.ejb.Stateless;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.persistence.TypedQuery;
 
 import net.dubrouski.fams.dao.PersonAddressDao;
+import net.dubrouski.fams.exception.FmsException;
 import net.dubrouski.fams.model.Person;
 import net.dubrouski.fams.model.PersonAddress;
 
@@ -19,8 +22,15 @@ import net.dubrouski.fams.model.PersonAddress;
 public class PersonAddressDaoImpl extends BaseDaoImpl<PersonAddress, Long>
 		implements PersonAddressDao {
 
+	@Inject
+	Logger logger;
+	
+	
 	@Override
 	public List<PersonAddress> getAddressesForPerson(Person person) {
+		if(person == null){
+			throw new FmsException("Person cannot be null");
+		}
 		TypedQuery<PersonAddress> query = this.entityManager
 				.createQuery(
 						"select adr from Person pers JOIN pers.addresses adr where pers.id = :id order by adr.active desc",
