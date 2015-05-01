af7fa52ebf639f6dddfc7f3cb6b6c61e4ba4bfc2
xprazak2 <oprazak@redhat.com>
2015-05-01 11:56:19 +0200

    accommodations details + delete

---
+++ src/main/java/net/dubrouski/fams/controller/accommodation/AccommodationDetailController.java
@@ -0,0 +1,44 @@
+package net.dubrouski.fams.controller.accommodation;
+
+import java.io.Serializable;
+import java.util.logging.Logger;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.primefaces.event.ToggleEvent;
+
+import net.dubrouski.fams.model.AccommodationUnit;
+
+@ManagedBean
+@SessionScoped
+public class AccommodationDetailController implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private AccommodationUnit unit;
+	
+	@Inject
+	Logger logger;
+	
+	@Produces
+	@Named
+	public AccommodationUnit getUnit(){
+		logger.info(unit.toString());
+		return unit;
+	}
+	
+	public String showDetail(AccommodationUnit u){
+		unit = u;
+		return "accommodation-detail";
+	}
+	
+	public void handleChildrenToggle(ToggleEvent event){
+		
+	}
+}
