2c5196493a03ff72f774a68557181590d7001cbd
stanislau.dubrouski <jeylkey@gmail.com>
2015-05-11 23:50:33 +0200

    person mgmt responsivness improved.

--- src/main/java/net/dubrouski/fams/model/Person.java
+++ src/main/java/net/dubrouski/fams/model/Person.java
@@ -173,4 +173,31 @@ public class Person implements Serializable {
 		return this.id + " " + this.getFirstName() + " " + this.getLastName();
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((legalId == null) ? 0 : legalId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (legalId == null) {
+			if (other.legalId != null)
+				return false;
+		} else if (!legalId.equals(other.legalId))
+			return false;
+		return true;
+	}
+	
+	
+
 }
