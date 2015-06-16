1e9aca1316363601b3d88ad90f74e16276e1e358
stanislau.dubrouski <jeylkey@gmail.com>
2015-06-16 23:55:38 +0200

    user roles for user module defined.

--- src/main/java/net/dubrouski/fams/model/enums/UserRoles.java
+++ src/main/java/net/dubrouski/fams/model/enums/UserRoles.java
@@ -5,5 +5,5 @@ package net.dubrouski.fams.model.enums;
  * @author Martin Jelínek (xjeline5)
  */
 public enum UserRoles {
-	persUser, persAdmin, contractUser, contractAdmin, accommUser, accommAdmin, client
+	persUser, persAdmin, contractUser, contractAdmin, accommUser, accommAdmin, usermoduleUser, usermoduleAdmin, client
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,11 +7,11 @@ ALTER SEQUENCE contract_code_seq INCREMENT 11 MINVALUE 1001 CACHE 1001 start 100
 SELECT setval('public.accommodation_unit_code_seq', 1001, true);
 ALTER SEQUENCE accommodation_unit_code_seq INCREMENT 11 MINVALUE 1001 CACHE 1001 start 1001;
 
-INSERT INTO USER_ROLE (ROLE_NAME) VALUES ('persUser'), ('contractUser'), ('accommUser'), ('persAdmin'), ('contractAdmin'), ('accommAdmin'), ('client');
+INSERT INTO USER_ROLE (ROLE_NAME) VALUES ('persUser'), ('contractUser'), ('accommUser'), ('persAdmin'), ('contractAdmin'), ('accommAdmin'), ('client'), ('usermoduleAdmin'), ('usermoduleUser');
 
 --USERS
 INSERT INTO FMS_USER (USERNAME, PASSWORD) VALUES ('admin', '6491cacf01b2e1c6d08a5609d2f570ea57d71ae7f06e0391276d70d935d29aa51888d566751aa36dc5e12e18da693ece36427c167e2a7a67e48aca8928ba3979');
 INSERT INTO FMS_USER (USERNAME, PASSWORD) VALUES ('user', '6491cacf01b2e1c6d08a5609d2f570ea57d71ae7f06e0391276d70d935d29aa51888d566751aa36dc5e12e18da693ece36427c167e2a7a67e48aca8928ba3979');
 
-INSERT INTO FMS_USER2USER_ROLE (USER_ID, USER_ROLE_ID) VALUES (1,4), (1,5), (1,6);
-INSERT INTO FMS_USER2USER_ROLE (USER_ID, USER_ROLE_ID) VALUES (2,1), (2,2), (2,3);
+INSERT INTO FMS_USER2USER_ROLE (USER_ID, USER_ROLE_ID) VALUES (1,4), (1,5), (1,6), (1,8);
+INSERT INTO FMS_USER2USER_ROLE (USER_ID, USER_ROLE_ID) VALUES (2,1), (2,2), (2,3), (2,9);
